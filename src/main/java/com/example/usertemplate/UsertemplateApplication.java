cb63ab2184222f11a871c4e82c23a8c40794029f
jongseo0703 <jongseo0703@naver.com>
2025-08-25 15:03:10 +0900

    🎉 Start : springboot-security-jwt-template

---
+++ src/main/java/com/example/usertemplate/UsertemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.usertemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsertemplateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UsertemplateApplication.class, args);
+  }
+
+}
