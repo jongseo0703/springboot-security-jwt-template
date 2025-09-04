28f0ab1b941d15177f72427c6f6a9b2d6df57317
jongseo0703 <jongseo0703@naver.com>
2025-09-04 17:10:16 +0900

    ✨ Feat : JPA 설정 클래스 생성

---
+++ src/main/java/com/example/usertemplate/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.example.usertemplate.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/** JPA의 설정을 담당하는 JpaConfig */
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
