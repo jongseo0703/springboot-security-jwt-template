b886ebbb567dd18eb64a9dab410ab65a7a358a99
jongseo0703 <jongseo0703@naver.com>
2025-09-04 17:16:42 +0900

    ✨ Feat : JPA를 사용하는 인터페이스인 UserRepository 생성

---
+++ src/main/java/com/example/usertemplate/user/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.example.usertemplate.user.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.usertemplate.user.entity.User;
+
+/** JPA를 사용하는 UserRepository임. */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  // username으로 존재하는지 체크.
+  boolean existsByUsername(String username);
+
+  // email로 존재하는지 체크.
+  boolean existsByEmail(String email);
+
+  // username으로 유저를 찾음.
+  Optional<User> findByUsername(String username);
+}
