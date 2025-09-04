e7ac2c15cf91ac8c5ad3e5b68de14fad8cd1b7f9
jongseo0703 <jongseo0703@naver.com>
2025-09-04 17:09:32 +0900

    ✨ Feat : 예외 처리를 담당하는 클래스 생성

---
+++ src/main/java/com/example/usertemplate/global/exception/ErrorResponse.java
@@ -0,0 +1,29 @@
+package com.example.usertemplate.global.exception;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+  private String result;
+  private String message;
+  private String errorCode;
+  private List<String> details;
+  private LocalDateTime timestamp;
+
+  public static ErrorResponse of(String message, String errorCode) {
+    return new ErrorResponse("ERROR", message, errorCode, null, LocalDateTime.now());
+  }
+
+  public static ErrorResponse of(String message, String errorCode, List<String> details) {
+    return new ErrorResponse("ERROR", message, errorCode, details, LocalDateTime.now());
+  }
+}
