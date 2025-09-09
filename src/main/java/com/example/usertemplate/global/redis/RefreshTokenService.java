b2fc4f8e8f6523e96763242f2458b1595be56d39
jongseo0703 <jongseo0703@naver.com>
2025-09-09 12:36:31 +0900

    ✨ Feat : RefreshToken과 관련된 로직을 처리하는 Service 생성

---
+++ src/main/java/com/example/usertemplate/global/redis/RefreshTokenService.java
@@ -0,0 +1,28 @@
+package com.example.usertemplate.global.redis;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+  private final RefreshTokenRepository refreshTokenRepository;
+
+  @Transactional
+  public void saveTokenInfo(String email, String refreshToken, String accessToken) {
+    refreshTokenRepository.save(new RefreshToken(email, accessToken, refreshToken));
+  }
+
+  @Transactional
+  public void removeRefreshToken(String accessToken) {
+    RefreshToken refreshToken =
+        refreshTokenRepository
+            .findByAccessToken(accessToken)
+            .orElseThrow(IllegalArgumentException::new);
+
+    refreshTokenRepository.delete(refreshToken);
+  }
+}
