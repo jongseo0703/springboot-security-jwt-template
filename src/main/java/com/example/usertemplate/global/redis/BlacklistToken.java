62794f454c0d929976efd6bc8a917826083197c7
jongseo0703 <jongseo0703@naver.com>
2025-09-09 12:36:57 +0900

    ✨ Feat : Blacklist에 등록하기 위한 Blacklist 생성

---
+++ src/main/java/com/example/usertemplate/global/redis/BlacklistToken.java
@@ -0,0 +1,17 @@
+package com.example.usertemplate.global.redis;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@RedisHash(value = "jwtBlacklist", timeToLive = 60 * 60 * 24)
+@Getter
+@AllArgsConstructor
+public class BlacklistToken implements Serializable {
+
+  @Id private String accessToken;
+}
