006fd99609a2570288d46731c596bbc709880c0f
jongseo0703 <jongseo0703@naver.com>
2025-09-12 17:28:13 +0900

    💡 Comments : 코드 주석 추가/수정

--- src/main/java/com/example/usertemplate/auth/oauth/dto/OAuthUserInfo.java
+++ src/main/java/com/example/usertemplate/auth/oauth/dto/OAuthUserInfo.java
@@ -2,9 +2,17 @@ package com.example.usertemplate.auth.oauth.dto;
 
 import lombok.Builder;
 
+/**
+ * OAuth 사용자의 정보를 전달하는 DTO
+ * @param oauthId
+ * @param email
+ * @param name
+ * @param provider
+ */
 @Builder
 public record OAuthUserInfo(String oauthId, String email, String name, String provider) {
 
+  // 구글에서 가져올 정보
   public static OAuthUserInfo fromGoogle(String oauthId, String email, String name) {
     return OAuthUserInfo.builder()
         .oauthId(oauthId)
@@ -14,6 +22,7 @@ public record OAuthUserInfo(String oauthId, String email, String name, String pr
         .build();
   }
 
+  // 네이버에서 가져올 정보
   public static OAuthUserInfo fromNaver(String oauthId, String email, String name) {
     return OAuthUserInfo.builder()
         .oauthId(oauthId)
@@ -23,6 +32,7 @@ public record OAuthUserInfo(String oauthId, String email, String name, String pr
         .build();
   }
 
+  // 카카오에서 가져올 정보
   public static OAuthUserInfo fromKakao(String oauthId, String email, String name) {
     return OAuthUserInfo.builder()
         .oauthId(oauthId)
